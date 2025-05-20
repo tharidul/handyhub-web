6cf41e9f183fc711f7bdaf7b468186c4aafa0915
tharidul <tharindulakmal5050@gmail.com>
2025-05-20 23:13:49 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Analytics } from '@vercel/analytics/react'
 import './App.css'
 
 function App() {
 
   return (
     <div>
-      <p className='text-5xl text-emerald-800 text-center'>Hello World</p>
+      <p className='text-5xl text-primary text-center'>Hello World</p>
+      <p className='text-5xl text-secondary text-center'>Hello World</p>
+      <p className='text-5xl text-navy text-center'>Hello World</p>
+      <p className='text-5xl text-blue text-center'>Hello World</p>
+      <p className='text-5xl text-darkgray text-center'>Hello World</p>
+      <Analytics />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ export const content = [
   "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",
 ];
+
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      primary: '#ffaa29',        // Vibrant orange
+      secondary: '#ffbd59',      // Soft orange/yellow
+      navy: '#001369',           // Deep navy blue
+      blue: '#004aad',           // Bold blue
+      white: '#ffffff',
+      darkgray: '#2e2e2e',       // Adjusted dark gray
+    },
+  },
 };
+
 export const plugins = [];
