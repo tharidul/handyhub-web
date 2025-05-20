a8cc5f31b94e33e712c4a66d2a6e633056d8aa60
tharidul <tharindulakmal5050@gmail.com>
2025-05-20 21:23:09 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 import './App.css'
 
 function App() {
 
   return (
     <div>
-      <p className='text-5xl'>Hello World</p>
+      <p className='text-5xl text-emerald-800 text-center'>Hello World</p>
     </div>
   )
 }
